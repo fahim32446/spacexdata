32f1de5170e01e4168631f5baac31657b86e0261
fahim32446 <azmirhasan.az2@gmail.com>
2023-01-25 15:40:01 +0600

    Start Working and Configuration Done

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import { DatePicker } from 'antd';
+
+function App() {
+  return (
+    <div className="App">
+      <DatePicker />
+    </div>
+  );
+}
+
+export default App;
