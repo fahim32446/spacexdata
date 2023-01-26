65c1548d26d16d01d7920e9f85a4e44f6c2f8bf9
fahim32446 <azmirhasan.az2@gmail.com>
2023-01-26 12:09:45 +0600

    Display Data into grid

---
+++ src/components/MissionCard.tsx
@@ -0,0 +1,49 @@
+import { Card } from 'antd';
+const { Meta } = Card;
+
+interface IProps {
+  item: {
+    details: string;
+    flight_number: Number;
+    is_tentative: Boolean;
+    links: {
+      mission_patch: string;
+      // mission_patch_small: string;
+      // reddit_campaign: string;
+      // reddit_launch: string;
+      // reddit_recovery: string;
+      // reddit_media: string;
+      // presskit: string;
+      // article_link: string;
+      // wikipedia: string;
+      // video_link: string;
+      // youtube_id: string;
+    };
+    launch_site: {
+      site_name: string;
+      site_name_long: string;
+    };
+  };
+}
+
+const MissionCard = ({ item }: IProps) => {
+  console.log(item);
+
+  const { links, launch_site } = item;
+
+  return (
+    <Card
+      style={{ width: 300 }}
+      hoverable={true}
+      cover={<img alt="Image" src={links.mission_patch} />}
+    >
+      <Meta
+        title={launch_site.site_name}
+        description={launch_site.site_name_long}
+      />
+    </Card>
+  );
+};
+
+export default MissionCard;
+
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,27 +1,38 @@
 import Heder from '../components/Header';
-import Card from '../components/Card';
-import {useGetMissionsQuery} from "../redux/spacexApi";
-const Home = () => {
-  const { data, isFetching, isLoading } = useGetMissionsQuery("");
+import MissionCard from '../components/MissionCard';
+import { useGetMissionsQuery } from '../redux/spacexApi';
+import { List } from 'antd';
 
-
- 
-  
-
-  console.log(data);
-
-
-  
-  return (
-    <div>
-        <Heder />
-
-      {isLoading ? "loading" : (
-        <Card />
-        
-      )}
-    </div>
-  )
+interface Item {
+  details: string;
+  flight_number: Number;
+  is_tentative: Boolean;
 }
 
-export default Home
+const Home = () => {
+  const { data, isLoading } = useGetMissionsQuery('');
+
+  return (
+    <div>
+      <Heder />
+      <List
+        loading={isLoading}
+        pagination={{
+          onChange: (page) => {
+            console.log(page);
+          },
+          pageSize: 12,
+        }}
+        grid={{ gutter: 16, column: 4 }}
+        dataSource={data}
+        renderItem={(item: Item) => (
+          <List.Item>
+            <MissionCard item={item as any} />
+          </List.Item>
+        )}
+      />
+    </div>
+  );
+};
+
+export default Home;
